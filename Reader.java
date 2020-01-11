e956dc3fba827c6c7992bb83f673f0f03cd75c37
Alexis-Mayers <42984987+Alexis-Mayers@users.noreply.github.com>
2020-01-11 16:05:50 -0500

    Add files via upload

---
+++ Reader.java
@@ -0,0 +1,10 @@
+// Program takes input of strings and parses them for the values
+
+public abstract class Read{
+  public String data;
+  public datainput[] input;
+
+
+
+}
+//https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
