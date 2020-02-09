21a69e8a99b5b9b6d40f29b099be993f7d1425d6
jksza042 <55514862+jksza042@users.noreply.github.com>
2020-02-08 21:19:04 -0500

    Update Reader.java

--- Reader.java
+++ Reader.java
@@ -1,21 +1,47 @@
 // Program takes input of strings and parses them for the values
-import java.io.File;
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.nio.file.*;
 
 public class Reader{
-  String info;
-  public static void main(String[] args) throws Exception
-  {
-    List<String[][]> list = new ArrayList<String[]>();
-    File file = new File("/Users/alexismayers/Desktop/FILE.txt");
-    Scanner s = new Scanner(file);
 
-    while (s.hasNextLine())
-      System.out.println(s.nextLine());
+  String info;
+  public static void main(String[] args) throws Exception{
+
+    String fileName = "FILE.txt";
+    ArrayList<Double> listR = new ArrayList<Double>();
+    ArrayList<Double> listV = new ArrayList<Double>();
+    ArrayList<Double> listX = new ArrayList<Double>();
+
+    Path path = Paths.get("./" + fileName);
+    String fileContents = Files.readString(path);
+
+    
+    String[] contList = fileContents.split(",");
+    String[] processingList;
+
+    for (int i = 0; i < contList.length; i++){
+      
+      processingList = contList[i].split("=");
+      processingList[0] = processingList[0].strip();
+
+      if (processingList[0].equals("r")){
+        listR.add(Double.parseDouble(processingList[1]));
+      }
+
+      if (processingList[0].equals("v")){
+        listV.add(Double.parseDouble(processingList[1]));
+      }
+
+      if (processingList[0].equals("x")){
+        listX.add(Double.parseDouble(processingList[1]));
+      }
 
     }
 
+    System.out.println("R:" + listR);
+    System.out.println("V:" + listV);
+    System.out.println("X:" + listX);
 
+  }
 }
 
